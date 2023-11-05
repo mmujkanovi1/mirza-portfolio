da514dd7addfec19cd71a72e798009e77cb48703
mirza <mirza@olivebh.com>
2023-11-05 23:03:58 +0100

    about, education page header footer finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
